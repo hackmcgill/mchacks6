57f5fee3c6680b586a7c2a56f8e27df96eeed191
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-11-10 20:02:08 -0500

    Force 1 accordion open at a time

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,8 +1,20 @@
 const items = document.querySelectorAll('.accordion__item');
 
+function closeAccordion() {
+  this.classList.remove('active');
+  Array.from(this.children).forEach(el => el.classList.remove('active'));
+}
+
 function toggleAccordion() {
+  if (!this.classList.contains('active')) {
+    Array.from(items)
+      .filter(i => i.classList.contains('active'))
+      .forEach(i => closeAccordion.bind(i)());
+  }
+
   this.classList.toggle('active');
   Array.from(this.children).forEach(el => el.classList.toggle('active'));
 }
+
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
