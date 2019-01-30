d1e0dbfb6dd3c7c68dbe96ce0be96fb994004305
Ryan Burgett <thesonicdoctor@gmail.com>
2019-01-30 18:26:13 -0500

    Fixed Maps
    Yeah

--- src/js/3tabs.js
+++ src/js/3tabs.js
@@ -7,16 +7,22 @@ $("document").ready(function(){
 $(".tab-slider--nav li").click(function() {
   $(".tab-slider--body").hide();
   var activeTab = $(this).attr("rel");
-  $("#"+activeTab).fadeIn();
+  // console.log(activeTab);
+  // $("#"+activeTab).fadeIn();
+  // $("#"+activeTab).show();
 	if($(this).attr("rel") == "tab2"){
     $('.tab-slider--tabs').removeClass('slide2');
     $('.tab-slider--tabs').addClass('slide1');
+    $(".tab-slider--body:nth-child(2)").show();
+    console.log()
 	}else if($(this).attr("rel") == "tab3"){
     $('.tab-slider--tabs').removeClass('slide1');
     $('.tab-slider--tabs').addClass('slide2');
+    $(".tab-slider--body:nth-child(3)").show();
   }else{
     $('.tab-slider--tabs').removeClass('slide1');
     $('.tab-slider--tabs').removeClass('slide2');
+    $(".tab-slider--body:nth-child(1)").show();
   }
   $(".tab-slider--nav li").removeClass("active");
   $(this).addClass("active");
